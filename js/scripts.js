08c18984ebed804a12b7232a19f598d40134bc17
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-09 11:04:47 -0700

    Add UI logic and business logic
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-
+function countToBy(countTo, countBy) {
+  for (let index= 0; index < countTo; index += countBy) {
+    $(".Output").append(index + " ");
+  }
+}
 
 
 $(document).ready(function() {
@@ -6,5 +10,6 @@ $(document).ready(function() {
   event.preventDefault();
   const countTo = parseInt($("#CountTo").val());
   const countBy = parseInt($("#CountBy").val());
-
-}
+  countToBy(countTo, countBy);
+  });
+});
