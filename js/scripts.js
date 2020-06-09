f88a6ac0aeae062da8cf0d2f945ecf67386b80cc
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-09 10:40:09 -0700

    Add all forms, HTML code, and code for user input on JS
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function() {
+  $("#Counter").submit(function(event) {
+  event.preventDefault();
+  const countTo = parseInt($("#CountTo").val());
+  const countBy = parseInt($("#CountBy").val());
+
+}
